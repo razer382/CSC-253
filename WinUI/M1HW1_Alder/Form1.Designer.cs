b398d405614234626c56d2a51cb45dc1fbc93a24
razer382 <67071120+razer382@users.noreply.github.com>
2021-08-18 17:07:46 -0400

    Add files via upload
    M1HW1

---
+++ WinUI/M1HW1_Alder/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace M1HW1_Alder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.temperatureListBox = new System.Windows.Forms.ListBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // temperatureListBox
+            // 
+            this.temperatureListBox.FormattingEnabled = true;
+            this.temperatureListBox.Location = new System.Drawing.Point(12, 12);
+            this.temperatureListBox.Name = "temperatureListBox";
+            this.temperatureListBox.Size = new System.Drawing.Size(245, 381);
+            this.temperatureListBox.TabIndex = 0;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(92, 415);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.temperatureListBox);
+            this.Name = "Form1";
+            this.Text = "C to F Table";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox temperatureListBox;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ WinUI/M1HW1_Alder/Form1.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * 8/18/2021
+ * CSC-253
+ * Branden Alder
+ * Program displaying a table of celsius converted to fahrenheit
+ */
+
+using System;
+using System.Windows.Forms;
+using TempConversionClassLibrary;
+
+namespace M1HW1_Alder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double celsius;         // To hold the celsius temperature
+            double fahrenheit;      // To hold the fahrenheit temperature
+
+            // Display a header for the table
+            temperatureListBox.Items.Add("Celsius\tFahrenheit");
+
+            // Display the table of temperatures
+            for (celsius = 0; celsius <= 20; celsius++)
+            {
+                fahrenheit = Conversion.ConvertToFahrenheit(celsius);
+                temperatureListBox.Items.Add(celsius + "\t" + fahrenheit);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        } // Closes program
+    }
+}
